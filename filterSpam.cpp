5aa8d598feb022dd98f301d269153878531a6720
moonlovestar <wayne80102@gmail.com>
2014-03-20 11:47:46 +0800

    Added classes in filterSpam.cpp

--- filterSpam.cpp
+++ filterSpam.cpp
@@ -2,10 +2,75 @@
 #include<vector>
 #include<fstream>
 #define STRING_LENGTH 256
+#define MAX_LENGTH 3
 using namespace std;
 
 vector<string> SMS;
 vector<string> SpamSMS;
+
+class PatternUnit{
+	private:
+		string patternName;
+		int freqOfOccur;
+	public:
+		PatternUnit(string patternName, int freq)
+		{
+			this.patternName=patternName;
+			this.freqOfOccur=freq;
+		}
+		string getPatternName()
+		{
+			return patternName;
+		}
+		int getFrequency()
+		{
+			return freqOfOccur;
+		}
+};
+class PatternContainer{
+	private:
+		vector<PatternUnit> pattern;
+	public:  
+		vector<PatternUnit> getPattern()
+		{
+			return pattern;
+		}
+		bool addPatternUnit(PatternUnit PU)
+		{
+			pattern.push_back(PU);
+			return 1;
+		}
+};
+class Database{
+	private: 
+		vector<PatternContainer> lengthPatternSet;
+	public:
+		vector<PatternContainer> getLengthPattern()
+		{
+			return lengthPatternSet;
+		}
+		bool addLengthPattern(PatternContainer PC , int length)
+		{
+			return 1;
+		}
+  
+};
+class BasicTrainingClass
+{
+	public:
+		virtual void training()=0;
+};
+class TrainingClass_Apriori:BasicTrainingClass
+{
+	private:
+		void training();
+};
+class TrainingClass_Sample:BasicTrainingClass
+{
+	private:
+		void training();
+};
+
 void readSMSTrainingData()
 {
 	fstream file;
