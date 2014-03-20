18e0124c1262be226e7e70f909e58dd6ee4e2e30
ericwalker <ericdavid78929@gmail.com>
2014-03-20 11:43:10 +0800

    add qtdemo

---
+++ qtdemo/qtdemo.cpp
@@ -0,0 +1,15 @@
+#include <QtGui/QApplication>
+#include <QtGui/QPushButton>
+ 
+int main(int argc, char *argv[]) {
+    QApplication application(argc, argv);
+    QPushButton button("Hello world");
+    button.show();
+    return application.exec();
+}
+ 
+/* 《程式語言教學誌》的範例程式
+    http://pydoing.blogspot.com/
+    檔名：qtdemo.cpp
+    功能：示範 C++ 程式 
+    作者：Qt 官網 */
